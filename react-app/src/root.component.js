7da26475917535b2cd4dce75724b120137a94312
Anuroop Joy <anuroop.joy@hrblock.com>
2021-10-07 16:00:24 +0530

    Init

---
+++ react-app/src/root.component.js
@@ -0,0 +1,9 @@
+export default function Root(props) {
+  return (
+    <section>
+      {props.name} is mounted!
+      <br />
+      <a href="/angular">Go to Angular app</a>
+    </section>
+  );
+}
